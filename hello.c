de1f2b6333621d864fc66caca43b6782553ec668
Murrium <murriumcra09@gmail.com>
2025-10-21 20:33:23 -0400

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//Variables 
+int main() {
+    int age = 0; //Enter your age
+    double money = 00.00; //Enter a financial amount but without the $ sign. 
+    char name[] = "Name" ; // Enter your name. 
+    printf("My name is %s, and I am %d years old. I have $%.2f in my bank account.\nNice to meet you %s... I hope we will meet again.",name,age,money,name);
+    return 0;
+}

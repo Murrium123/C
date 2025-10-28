4442f07562fa60e2c6ad273dcb76779973f3c73f
Murrium <murriumcra09@gmail.com>
2025-10-27 23:32:58 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 //Variables 
 int main() {
-    int age = 0; //Enter your age
-    double money = 00.00; //Enter a financial amount but without the $ sign. 
-    char name[] = "Name" ; // Enter your name. 
+    int age = 0; //Enter your age -- This is an integer
+    double money = 00.00; //Enter a financial amount but without the $ sign -- This is a double
+    char name[] = "Name" ; // Enter your name -- This is a String array or an array of Chars
     printf("My name is %s, and I am %d years old. I have $%.2f in my bank account.\nNice to meet you %s... I hope we will meet again.",name,age,money,name);
     return 0;
+
 }

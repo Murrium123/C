0ede1e63cb3ee05dc49593d66e1ae140e63ef97b
Murrium <murriumcra09@gmail.com>
2025-10-28 17:47:02 -0400

    Add files via upload

---
+++ condition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int
+main (void)
+{
+      int grade =50;
+      
+      if (grade > 60){
+        printf("You passed! Your grade is %d\n",grade);
+        }
+      else{
+        printf("I am sorry buddy, you didn't make it this time. ");
+      }
+	return (0);
+}

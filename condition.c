6e5e8ca3bfc924957f771f78b06a2612b82c68d0
Murrium <murriumcra09@gmail.com>
2025-10-29 11:25:03 -0400

    Update condition.c

--- condition.c
+++ condition.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
-int
-main (void)
-{
+int main (void) {
       int grade =50;
-      
-      if (grade > 60){
-        printf("You passed! Your grade is %d\n",grade);
-        }
-      else{
-        printf("I am sorry buddy, you didn't make it this time. ");
-      }
+      if (grade > 60){printf("You passed! Your grade is %d\n",grade);}
+      else{printf("I am sorry buddy, you didn't make it this time. ");}
 	return (0);
 }

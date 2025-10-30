1ffdec3f82faec51e0f3a85135e1e878c5dbcb62
Murrium <murriumcra09@gmail.com>
2025-10-30 10:08:55 -0400

    Update loops.c

--- loops.c
+++ loops.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-#define COUNT_TO 100
 
 int
 main (void)
 {
-   int n;
- 
-   n = 1; /* loop initialization */
- 
    printf("For loop\n");   
-   for(int i = 0; i < 100; i++){
-      printf("%d ", i);
+   for(int i = 0; i < 100; i++){ /*Loop initialization; condition; increments/decrements*/
+      printf("%d ", i); //Printing the Variable
     }
-   printf("\n While loop\n");       
-   while(n <= COUNT_TO){
-      printf ("%d ", n);
+   
+   printf("\n While loop\n");    
+   int n = 0; /* loop initialization */
+   while(n <= 100){ /*While n is less than or equal to 100*/
+      printf ("%d ", n); /**/
       n = n + 1;
    }
+   
    printf("\n Do While loop\n");  
    n=0;
    do{

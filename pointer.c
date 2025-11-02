6319801594ae157a014afff704c4a4bb514d4e9f
Murrium <murriumcra09@gmail.com>
2025-11-02 12:53:50 -0500

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main (void) {
-   int a = 5;
-   int* aptr = &a;
+   int a = 5; //Integer variable set up. 
+   int* aptr = &a; //Integer pointer set up.
    printf("My variable is called %s and it is an integer with the value of %d. The pointer for this variable is called %s which holds the memory address of my variable which is %p. It itself is stored at %p", "a", a, "aptr", aptr, &aptr );
-   return (0);
+   return (0); //Return is set to zero because it lets the compiler know that this code should run successfully. 
 }
